83e8589035a1d1f3016e0f6ebf7810bfda5f8487
Eric Nantz <thercast@gmail.com>
2024-08-23 15:19:22 -0400

    begin shinytest2 setup

---
+++ tests/testthat/apps/basic/app.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(shinystate)
+
+storage <- StorageClass$new()
+
+ui <- function(request) {
+  fluidPage(
+    use_shinystate(),
+    textInput("txt", "Enter text"),
+    checkboxInput("caps", "Capitalize"),
+    verbatimTextOutput("out"),
+    actionButton("bookmark", "Bookmark"),
+    actionButton("restore", "Restore Last Bookmark")
+  )
+}
+
+server <- function(input, output, session) {
+  storage$register_metadata()
+  output$out <- renderText({
+    if (input$caps) {
+      toupper(input$txt)
+    } else {
+      input$txt
+    }
+  })
+
+  observeEvent(input$bookmark, {
+    storage$snapshot()
+    showNotification("Session successfully saved")
+  })
+
+  observeEvent(input$restore, {
+    session_df <- storage$get_sessions()
+    storage$restore(tail(session_df$url, n = 1))
+  })
+
+  setBookmarkExclude(c("bookmark", "restore"))
+}
+
+shinyApp(ui, server, onStart = function() {
+  shiny::enableBookmarking("server")
+})
