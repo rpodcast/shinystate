99b1625899c31cb8edc915129f5864764dd94eda
Eric Nantz <theRcast@gmail.com>
2024-08-22 23:57:07 -0400

    refresh nix dev environment setup

---
+++ prototyping/build.R
@@ -0,0 +1,10 @@
+library(rix)
+rix(
+  r_ver = "latest",
+  r_pkgs = c("shiny", "fs", "languageserver", "pins", "archive", "s3fs", "DT", "DBI", "RSQLite", "lubridate", "pool", "dplyr", "dbplyr", "devtools", "testthat", "arrow", "Microsoft365R", "paws_storage", "sodium", "zip", "AzureStor",  "shinytest2", "chromote"),
+  system_pkgs = NULL,
+  git_pkgs = NULL,
+  ide = "radian",
+  project_path = ".",
+  overwrite = TRUE
+)
