de6665eedfc9f0d5a69822fb9ff952a44634a4cc
Eric Nantz <thercast@gmail.com>
2024-10-17 13:56:58 -0400

    Fix typo in URL pattern in test for session ID extraction

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# shinystate (development version)
+
+* Fix typo in test for session ID extraction
+
 # shinystate 0.0.0.9000
 
 * Create initial version of R6 class `StorageClass`
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,5 @@
 test_that("session ID extracton from URL works", {
-  url <- "http://127.0.0.1/?state_id_=1234567890fa"
+  url <- "http://127.0.0.1/?_state_id_=1234567890fa"
   expect_identical(session_id_from_url(url), "1234567890fa")
 })
 
