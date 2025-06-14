c52b61041be3f6058a99f13ec8a8ba5f0cb6dc08
Eric Nantz <theRcast@gmail.com>
2025-06-14 00:23:43 -0400

    chore: move rix R script for building Nix config to root dir

---
+++ build_nixconfig.R
@@ -0,0 +1,37 @@
+#https://gist.github.com/b-rodrigues/d427703e76a112847616c864551d96a1
+library(rix)
+
+rix(
+  date = "2025-04-16",
+  #r_ver = "4.4.2",
+  project_path = getwd(),
+  r_pkgs = c(
+    "archive",
+    "cli",
+    "DBI",
+    "devtools",
+    "dplyr",
+    "DT",
+    "fs",
+    "htmltools",
+    "lubridate",
+    "pins",
+    "R6",
+    "RSQLite",
+    "pool",
+    "shiny",
+    "tibble",
+    "rlang",
+    #"shinytest2",
+    "testthat",
+    "withr",
+    "s3fs",
+    "sodium",
+    "shinyvalidate",
+    "chromote",
+    "zip",
+    "ggplot2"
+  ),
+  ide = "none",
+  overwrite = TRUE
+)
